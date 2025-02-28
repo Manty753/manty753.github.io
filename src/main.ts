c0db1c93fbc0c1ca2a494864371564bbf9dfdc46
Jakub Novak <kubanovak1561995@gmail.com>
2025-02-28 09:17:00 +0100

    swagger ui absolute paths

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
+
   const config = new DocumentBuilder()
     .setTitle('Customer Evidence Server')
     .setDescription('The customer evidence server API')
@@ -18,7 +19,14 @@ async function bootstrap() {
     .addTag('Customers')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+  SwaggerModule.setup('api', app, documentFactory, {
+    customSiteTitle: 'API Docs',
+    customCssUrl: 'https://unpkg.com/swagger-ui-dist/swagger-ui.css',
+    customJs: [
+      'https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js',
+      'https://unpkg.com/swagger-ui-dist/swagger-ui-standalone-preset.js',
+    ],
+  });
 
   await app.listen(process.env.PORT ?? 3000);
   console.log(`Server is running on port ${process.env.PORT || 3000}`);
